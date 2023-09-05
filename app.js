23ab59b20efbd0321bcab6eae986de2fd1ba5fc1
feliperosenek <feliperosenek@gmail.com>
2023-09-05 12:18:41 -0300

    deploy

--- app.js
+++ app.js
@@ -14,16 +14,32 @@ api.use(bodyParser.json())
 function removerCaracteresAposBr(email) {
     const novoEmail = email.replace(/\.br.*/, '.br');
     return novoEmail;
-  }
+}
 
 api.get('/', (req, res) => {
     const form = `<!DOCTYPE html>
     <html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <body>
-        <h1>Formulário POST</h1>
+        <style>
+        body {
+            padding: 100px;
+        }
+        
+        h1 {
+            margin-bottom: 34px;
+        }</style>
+        <h1 class="text-center" >Pesquisa E-mails Google</h1>
         <form action="/" method="POST">
-            <textarea id="mensagem" name="data" rows="4" cols="50" required></textarea><br><br>
-            <input type="submit" value="Enviar">
+        <div class="form-group">
+        <textarea class="form-control" name="data" rows="5" cols="5"></textarea>
+      </div>
+      <div style="margin-top:30px" class="row">
+      <div class="col-md-12 text-center">
+      <button type="submit" class="btn btn-primary btn-lg">Pesquisar</button>
+        </div>
+        </div>
         </form>
     </body>
     </html>
